0aa0306625e65b30aa8e86f674502dfe619b4e67
Harsha2717 <ncsriharsha90@gmail.com>
2023-11-27 16:12:37 +0530

    Small change to the Functions File

--- JS_Practice/Functions/Calculator.js
+++ JS_Practice/Functions/Calculator.js
@@ -1,1 +1,32 @@
-//Here we use the 
+//Here we use the functions to calculate the Addition substracion and multiplication 
+function Addition(num1 ,num2)
+{
+    return num1+num2;
+}
+function substraction(num1 ,num2)
+{
+    return num1 -num2;
+}
+
+function multiplication(num1,num2)
+{
+    return num1*num2;
+}
+function Division(num1 , num2 )
+{
+    return num1/num2;
+}
+
+let num1 = 10;
+let num2 = 20;
+let add = Addition(num1,num2);
+let sub = substraction(num1,num2);
+let multi = multiplication(num1,num2);
+let div = Division(num1,num2);
+
+console.log(`
+Addition of ${num1} + ${num2} = ${add}
+Substraction of ${num1} - ${num2} = ${sub}
+Multiplication of ${num1} * ${num2} = ${multi}
+Division of ${num1} / ${num2} = ${div}`);
+
